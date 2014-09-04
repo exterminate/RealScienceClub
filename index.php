17b8334f5aa5769be0734ed7e68f46cada2b6bb8
Ian <ian.coates@gmail.com>
2014-09-04 11:27:08 +0100

    create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<form action="" method="POST">
+	<label>Name</label><br>
+	<input type="text" name="name" placeholder="name..."><br>
+	<label>No. of decisions</label><br>
+	<input type="text" name="number" size="2"><br>
+	<input type="submit" name="submit" value="submit">
+</form>
