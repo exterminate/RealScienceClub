d7d5c361651189e7d6830f451fe713f03edd9577
Ian <ian.coates@gmail.com>
2014-09-04 11:54:02 +0100

    update

--- show.php
+++ show.php
@@ -3,7 +3,7 @@
 $file = unserialize(file_get_contents("decisionrecord.json"));
 
 foreach($file as $k=>$f){
-	echo "<li>$f['name'] - $f['number'] - <a href="?i=$k">mark done</a></li>";
+	echo "<li>".$f['name']." - ".$f['number']." - <a href='?i=".$k."'>mark done</a></li>";
 }
 
 
