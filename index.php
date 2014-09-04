e8ada0f27bd1ca67f2e8d136b165ea361f3930e1
Ian <ian.coates@gmail.com>
2014-09-04 11:31:42 +0100

    update

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+
+if(isset($_POST['submit'])) {
+	if(isset($_POST['number']) && isset($_POST['name'])) {
+		
+		$_SESSION['flash'] = "Thanks";
+	}
+	else {
+		$_SESSION['flash'] = "Oops something wend wrong try again";
+	}
+}
+
+?>
+
 <form action="" method="POST">
 	<label>Name</label><br>
 	<input type="text" name="name" placeholder="name..."><br>
@@ -5,3 +19,12 @@
 	<input type="text" name="number" size="2"><br>
 	<input type="submit" name="submit" value="submit">
 </form>
+
+<?php
+// flash
+
+if(isset($_SESSION['flash'])) {
+	echo $_SESSION['flash'];
+	unset($_SESSION['flash']);
+}
+?>
