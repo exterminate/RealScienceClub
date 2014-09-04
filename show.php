f8105b3fef70965a987900052190393c80ddadb0
Ian <ian.coates@gmail.com>
2014-09-04 11:47:41 +0100

    create

---
+++ show.php
@@ -0,0 +1,13 @@
+<?php
+
+$file = unserialize(file_get_contents("decisionrecord.json"));
+
+foreach($file as $k=>$f){
+	echo "<li>$f['name'] - $f['number'] - <a href="?i=$k">mark done</a></li>";
+}
+
+
+
+header("Location: show.php");
+
+?>
